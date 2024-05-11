cf29f58e42f46c6b1ac0cbed2914fe2263772f35
mariamlelashvili <dalijananashvilli@gmail.com>
2024-05-11 20:18:53 +0400

    commit

---
+++ Day 01/homework/homework.py
@@ -0,0 +1,34 @@
+from turtle import *
+
+
+
+print("mariam lelashvilli")
+print("150")
+print("GOA ში შემოვედი რადგან დამაინტერესდა პროგრამირება და გადავწყვიტე GOA ში შემოვსულიყავი " )
+print("11 წლის")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exitonclick()
